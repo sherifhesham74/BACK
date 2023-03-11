7e684674d9e51c1ed6f68ead39ce5cb128760e11
sherifhesham74 <100443942+sherifhesham74@users.noreply.github.com>
2023-03-11 22:14:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const cors = require("cors");
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://kalaks.onrender.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
