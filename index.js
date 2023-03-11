98a95c82e7e2962bb9b7e06569d0ca5c2e61af83
sherifhesham74 <100443942+sherifhesham74@users.noreply.github.com>
2023-03-11 22:03:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,25 @@ const cors = require("cors");
 //   origin: "http://localhost:3000",
 //   optionsSuccessStatus: 200,
 // };
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://kalaks.onrender.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // app.get("/" , (req,res)=>{
 //   res.setHeader("Access-Control-Allow-Credentials","true");
