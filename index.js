d1aae7a5aaa80fed7585629ea324c73723917267
sherifhesham74 <100443942+sherifhesham74@users.noreply.github.com>
2023-03-12 03:27:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,24 @@ app.use("/schools", schoolsRouter);
 app.use("/maintains", maintainRouter);
 app.use("/accessShops", accessShopRouter);
 app.use("/accessories", accessRouter);
-app.use(cors({ origin : '*' }));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin: '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // app.use("/cart", cartRouter);
 app.get("/", (req, res) => {
   res.json({ mssg: "Welcom to the app" });
@@ -58,24 +75,7 @@ app.listen(process.env.PORT, () => {
 // };
 // Add headers before the routes are defined
 
-// app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
 
 // app.get("/" , (req,res)=>{
 //   res.setHeader("Access-Control-Allow-Credentials","true");
